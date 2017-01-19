ade05abdc8315d1d99712cb51bda2b4c372ed75d
Mike Bublitz <m.bublitz@shaw.ca>
2017-01-18 21:03:08 -0700

    RSVP form

---
+++ partyinvites-webapp/src/main/java/ca/nait/controller/GuestResponse.java
@@ -0,0 +1,5 @@
+package ca.nait.controller;
+
+public class GuestResponse {
+
+}
---
+++ partyinvites-webapp/src/main/java/ca/nait/controller/RsvpFormController.java
@@ -0,0 +1,33 @@
+package ca.nait.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import ca.nait.domain.GuestResponse;
+
+//mark this class as a JSF CDI-managed bean class
+@Named //allows you to access an instance of this class using JSF-EL (Expression Language)
+@RequestScoped  // create an instance for one HTTP request and destroy it after the HTTP response is sent
+				// other scopes:
+				// @ViewedScoped //Keep instance until HTTP request for another page
+				// @SessionScoped //allows instance to be shared across multiple pages for one client
+				// @ApplcationScoped //one instance is shared with ALL clients (global data)
+public class RsvpFormController {
+
+	private GuestResponse guestResponse = new GuestResponse(); //getter and setter
+	
+	public GuestResponse getGuestResponse() {
+		return guestResponse;
+	}
+
+	public void setGuestResponse(GuestResponse guestResponse) {
+		this.guestResponse = guestResponse;
+	}
+
+	public String submitForm()
+	{
+		return "/thanks";
+	}
+	
+	
+}
